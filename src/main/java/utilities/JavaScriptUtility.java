c40dd5ef0655bac6f67196442e7391789a2c883e
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-09-12 23:58:02 +0530

    Working With Radio Buttons

--- src/main/java/utilities/JavaScriptUtility.java
+++ src/main/java/utilities/JavaScriptUtility.java
@@ -9,6 +9,11 @@ public class JavaScriptUtility extends  Utility{
         WebElement element = driver.findElement(locator);
         String jsScript = "arguments[0].scrollIntoView();";
         ((JavascriptExecutor)driver).executeScript(jsScript, element);
+    }
 
+    public static void clickJS(By locator){
+        WebElement element = driver.findElement(locator);
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element);
     }
 }
